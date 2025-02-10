c55458650042186c51c1f3060412747fa1e140b6
Pr3dat0rBRG <145338767+Pr3dat0rBRG@users.noreply.github.com>
2025-02-10 15:30:36 +0300

    Update srm.sql

--- srm.sql
+++ srm.sql
@@ -1,1 +1,7 @@
-postgres=# SELECT name, age FROM person WHERE address='Kazan';
+-- Задание 1
+SELECT name, age FROM person WHERE address='Kazan';
+-- Задание 2
+SELECT name, age FROM person WHERE address='Kazan' AND gender='female' ORDER BY name;
+-- Задание 3
+SELECT name, rating FROM pizzeria WHERE rating>=3.5 AND rating<=5 ORDER BY rating;
+SELECT name, rating FROM pizzeria WHERE rating BETWEEN 3.5 AND 5 ORDER BY rating;
